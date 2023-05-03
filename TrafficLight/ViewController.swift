ae186bff8240d3a0bfe5264f0c0430a29c646256
NordStj <Neoskyping@gmail.com>
2023-05-03 10:27:51 +0300

    final product

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -12,38 +12,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var yellowView: UIView!
     @IBOutlet weak var greenView: UIView!
     @IBOutlet weak var buttonRefrashLight: UIButton!
+    let lightOn: CGFloat = 1
+    let lightOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let radiusCircleView = redView.frame.size.width / 2
-        // setup circle form for view
-        redView.layer.cornerRadius = radiusCircleView
-//        redView.clipsToBounds = true
-//        yellowView.layer.cornerRadius = radiusCircleView
-//        yellowView.clipsToBounds = true
-//        greenView.layer.cornerRadius = radiusCircleView
-//        greenView.clipsToBounds = true
+        
+        
         // setup alpha for view
-        redView.alpha = 0.3
-        yellowView.alpha = 0.3
-        greenView.alpha = 0.3
+        redView.alpha = lightOff
+        yellowView.alpha = lightOff
+        greenView.alpha = lightOff
+        
+//        redView.layer.cornerRadius = redView.frame.width / 2
+//        yellowView.layer.cornerRadius = redView.frame.width / 2
+//        greenView.layer.cornerRadius = redView.frame.width / 2
+        
+        
         
         buttonRefrashLight.setTitle("START", for: .normal)
         buttonRefrashLight.layer.cornerRadius = 10
     }
-
+    override func viewWillLayoutSubviews() {
+        // setup circle form for view
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = redView.frame.width / 2
+        greenView.layer.cornerRadius = redView.frame.width / 2
+    }
     @IBAction func buttonUpdate() {
-        if redView.alpha == 1 {
-            yellowView.alpha = 1
-            redView.alpha = 0.3
-        } else if yellowView.alpha == 1 {
-            greenView.alpha = 1
-            yellowView.alpha = 0.3
-        } else if greenView.alpha == 1 {
-            redView.alpha = 1
-            greenView.alpha = 0.3
+        if redView.alpha == lightOn {
+            yellowView.alpha = lightOn
+            redView.alpha = lightOff
+        } else if yellowView.alpha == lightOn {
+            greenView.alpha = lightOn
+            yellowView.alpha = lightOff
+        } else if greenView.alpha == lightOn {
+            redView.alpha = lightOn
+            greenView.alpha = lightOff
         } else {
-            redView.alpha = 1
+            redView.alpha = lightOn
         }
         buttonRefrashLight.setTitle("NEXT", for: .normal)
     }
